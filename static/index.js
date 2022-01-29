3397f9a836f8e77a2102a6cf657c40213fef661b
zhengboze6 <98633892+zhengboze6@users.noreply.github.com>
2022-01-29 15:25:06 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,10 +441,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 5) return '不要昨壁！';
+        if (cps <= 8) return '小测没过';
+        if (cps <= 10)  return '昨夜没作';
+        if (cps <= 15) return '下面的思或';
         return '人？';
     }
 
